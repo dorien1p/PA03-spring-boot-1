b50601b8985b6a67ac21e15259285efd2beb86ad
Dorien Parris <DORIEN1.PARRIS@FAMU.EDU>
2026-04-26 23:27:58 -0400

    Initial Spring Boot setup with DTO and Store

---
+++ src/test/java/edu/famu/cop3060/yard/YardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.famu.cop3060.yard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
